3b9190ebfc2955a3b81fa39e9ed26e44dee9ae37
Olavo Neves <olavo9neves@gmail.com>
2025-04-25 12:18:03 -0300

    ligando banco de dados

--- gerenciador-pedidos/src/main/java/br/com/gerenciadordepedidos/model/Produto.java
+++ gerenciador-pedidos/src/main/java/br/com/gerenciadordepedidos/model/Produto.java
@@ -1,4 +1,25 @@
 package br.com.gerenciadordepedidos.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nome;
+    @Column(name = "valor")
+    private double preco;
+
+    public Produto() {
+
+    }
+
+    public Produto(Long id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
 }
